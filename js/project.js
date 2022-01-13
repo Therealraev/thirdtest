fe13a7f8717b068158aef91c7d32cf25cd81f28a
Feruz <feruzjuraev6@gmail.com>
2022-01-13 22:18:36 +0500

    first commit

---
+++ js/project.js
@@ -0,0 +1,51 @@
+"use strict";
+/* 
+const obj = {
+   name: "Feruz",
+   age: 20,
+   isMarried: false
+ };
+console.log(obj["isMarried"]);
+let arr = [
+   'plum.pnj',
+   'orange.jpg', 
+   'apple.bmp',
+   {
+      name:"Feruz",
+      age: 20,
+   }
+];
+console.log(arr[3]);
+*/
+
+/*
+function funcon() { 
+// сообщение alert("Привет");
+// вопрос с кратким ответом const t = confirm("Are you here?");
+console.log(t);
+if(t == false){
+   alert("Вас тут нету повердите вашу личность");
+  // Ответ на вопрос  let answer = prompt("Как вас зовут?", "");
+   if(answer == "Feruz"){
+      alert("Личность подверждена");
+   }
+   else{
+      alert("Это не вы система блокирует вас через 10 секунд");
+   }
+}
+ }
+ */
+/*
+const answers = [];
+answers [0] = prompt("Как вас зовут ? ", "");
+answers [1] = prompt("Как ваше фамилия ? ", "");
+answers [2] = prompt("Как сколько вам лет ? ", "");
+document.write(answers);
+*/
+
+const category = 'toys';
+console.log(`https://someurl/${category}/4`);
+
+const isChecked = true,
+      isWillcheck = true;
+      console.log(!isChecked && isWillcheck);
